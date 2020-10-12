cde0c48951632418dae80cf754ce9727b1549402
amiirds <amiirds44>
2020-10-12 21:08:55 +0330

    commit

---
+++ Homework/s4/ex3/src/com/ds/usc/Main.java
@@ -0,0 +1,35 @@
+package com.ds.usc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        student_information student_information = new student_information();
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your fullname");
+        student_information.setFullname(input.nextLine());
+        System.out.println("enter your fathername");
+        student_information.setFathername(input.nextLine());
+        System.out.println("enter student number");
+        student_information.setStudent_number(input.nextLine());
+        System.out.println(student_information.getFullname());
+        List <Double> grade = new ArrayList<>();
+        System.out.println("enter your first grade");
+        grade.add(input.nextDouble());
+        System.out.println("enter your second grade");
+        grade.add(input.nextDouble());
+        System.out.println("enter your third grade");
+        grade.add(input.nextDouble());
+        System.out.println("enter your fourth grade");
+        grade.add(input.nextDouble());
+        System.out.println("enter your fifth grade");
+        grade.add(input.nextDouble());
+        System.out.println(student_information.getFullname());
+        System.out.println(student_information.getFathername());
+        System.out.println(student_information.getStudent_number());
+
+    }
+}
---
+++ Homework/s4/ex3/src/com/ds/usc/student_information.java
@@ -0,0 +1,31 @@
+package com.ds.usc;
+
+public class student_information<fullname,student_number,fathername> {
+    private fullname fullname;
+    private student_number student_number;
+    private fathername fathername;
+
+    public fullname getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(fullname fullname) {
+        this.fullname = fullname;
+    }
+
+    public student_number getStudent_number() {
+        return student_number;
+    }
+
+    public void setStudent_number(student_number student_number) {
+        this.student_number = student_number;
+    }
+
+    public fathername getFathername() {
+        return fathername;
+    }
+
+    public void setFathername(fathername fathername) {
+        this.fathername = fathername;
+    }
+}
