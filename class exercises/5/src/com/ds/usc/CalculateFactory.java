65bac988788bdac1bf231be1bba865ec4b2c57f1
amiirds <amiirds44>
2020-10-26 18:43:27 +0330

    commit

--- class exercises/5/src/com/ds/usc/CalculateFactory.java
+++ class exercises/5/src/com/ds/usc/CalculateFactory.java
@@ -2,13 +2,10 @@ package com.ds.usc;
 
 public class CalculateFactory {
     public Calculate getcalc(String c){
-        switch (c){
-            case "+" :
-                return new CalPlus();
-            case "-" :
-                return new CalMinus();
-            default:
-                return null;
-        }
+        return switch (c) {
+            case "+" -> new CalPlus();
+            case "-" -> new CalMinus();
+            default -> null;
+        };
     }
 }
