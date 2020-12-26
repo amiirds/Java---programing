ef7d9e07f484a31ca63a4a923bd557f6c89956e7
amiirds <amiirds44>
2020-12-26 06:35:38 +0330

    commit

---
+++ class exercises/11/src/com/ds/controller/EmployeeInfo.java
@@ -0,0 +1,31 @@
+package com.ds.controller;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("/EmployeeInformation")
+public class EmployeeInfo {
+    @Path("/info")
+    @GET
+    @Produces("text/plain")
+    public String save (@QueryParam("Full Name") String name , @QueryParam("Gender") String gender , @QueryParam("Age") String age ,@QueryParam("Education") String education )
+    {
+        System.out.println("form executed in server");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("fullname", name);
+        jsonObject.put("gender",gender);
+        jsonObject.put("age", age);
+        jsonObject.put("education",education);
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(jsonObject);
+
+        return jsonArray.toJSONString();
+    }
+
+}
---
+++ class exercises/11/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: amirreza
+  Date: 17/16/2020
+  Time: 7:11 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>amirreza Ds</title>
+  </head>
+  <body>
+  <h1>end</h1>
+  </body>
+</html>
